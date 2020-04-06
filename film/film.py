83044e99fc43e1a97665bbe3777acbc086b7709b
NarekBektashyan <46633057+NarekBektashyan@users.noreply.github.com>
2020-04-06 15:19:47 +0400

    Update film.py

--- film/film.py
+++ film/film.py
@@ -2,11 +2,15 @@ from csv import DictReader
 import random 
 import os
 import requests
-import wikipedia
 from colorama import Fore, Style
+from bs4 import BeautifulSoup
+
 
 kirill = ('абвгдеёжзийклмнопрстуфхцчшщъыьэюя')	
-movie = wikipedia.set_lang('ru')
+header = {
+	'x-fx-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImp0aSI6ImZ4LTVlOGFkNmVmMDY4OGMifQ.eyJpc3MiOiJodHRwczpcL1wvZmlsbWl4Lm1lIiwiYXVkIjoiaHR0cHM6XC9cL2ZpbG1peC5tZSIsImp0aSI6ImZ4LTVlOGFkNmVmMDY4OGMiLCJpYXQiOjE1ODYxNTcyOTUsIm5iZiI6MTU4NjE0NjQ5NSwiZXhwIjoxNTg4NzQ5Mjk1LCJwYXJ0bmVyX2lkIjoiMiIsImhhc2giOiIyMWZiYjI1NTM5ZTg2OTQzMDg1M2M4YjExZGY0MTZhOTkxODZlZjhjIiwidXNlcl9pZCI6bnVsbCwiaXNfcHJvIjpmYWxzZSwiaXNfcHJvX3BsdXMiOmZhbHNlLCJzZXJ2ZXIiOiIifQ.gCUVS0Mj4i63wAzfeDq-Vnap5pUTFFDRCE9pCz0VY6U'
+	
+}
 
 genre = {
 	 '1':'Drama','2': 'Comedy','3': 'Thriller', '4': 'Action', '5': 'Crime',
@@ -20,6 +24,35 @@ genre_russian = {
 	'10': 'Историческая', '11': 'Биография', '12': 'Военная', '13': 'Документальная'
 }
 
+def translate_title(title, year):
+	title = '+'.join(title.split())
+	year = str(year)
+	res = requests.get(f'https://ru.wikipedia.org/w/index.php?search={title}+фильм%2C{year}')
+	res = res.text.split('<')
+	new = [x for x in res if 'data-serp-pos' in x ]
+	link = new[0].split('"')[1]
+	res = requests.get('https://ru.wikipedia.org' + link)
+	title = [x for x in res.text.split('\n') if 'title' in x]
+	title_html = title[0]
+	cleantext = BeautifulSoup(title_html, "lxml").text
+	start = cleantext.find( '(' )
+	if start != -1:
+ 		cleantext = cleantext[:start]
+	if cleantext.find('—') > 0:
+		cleantext = cleantext.replace('—', '')
+	if cleantext.find('Википедия') > 0:
+		cleantext = cleantext.replace('Википедия', '')	
+	title = cleantext.strip()
+	return title
+
+def get_description(title):
+	res = requests.get(f'http://85.239.42.191/partner_api/suggests?query={title}', headers=header)
+	res = res.json()
+	movie_id = str([x for  x in res['items'] if x['title'] == title][0]['id'])
+	details = requests.get(f'http://85.239.42.191/partner_api/film/{movie_id}/details', headers=header)
+	html_details = details.json()['short_story']
+	description = BeautifulSoup(html_details, "lxml").text
+	return description
 
 choice = '1'
 
@@ -40,24 +73,17 @@ while choice != 'q':
 		title, year = movie.split(',')[:2]
 		title = '+'.join(title.split())
 		year = str(year)
-		res = requests.get(f'https://ru.wikipedia.org/w/index.php?search={title}%2C{year}')
-
-		res = res.text.split('<')
-
-		new = [x for x in res if 'data-serp-pos' in x ]
-
-		link = new[0].split('"')[1]
-		res = requests.get('https://ru.wikipedia.org' + link)
-		title = [x for x in res.text.split('<') if 'title' in x]
-		title = title[0].split('>')[1].split('—')[0]
+		title = translate_title(title, year)
 		find_kirill = [x for x in kirill if x in title.lower()]
 		if not find_kirill:
 			continue
-		description = wikipedia.summary(f"{title}(фильм)")
-		print('\nСоветую этот фильм.\n')
-		print(Fore.GREEN + title + ', ' + year)
-		print()
-		print(Style.RESET_ALL)
-		print(description)
+		try:
+			description = get_description(title)
+			print('\nСоветую этот фильм.\n')
+			print(Fore.GREEN + title + ', ' + year)
+			print()
+			print(Style.RESET_ALL)
+			print(description)
+		except Exception:
+			continue
 		choice = input('\nНайти другую?("y/n")Менять жанр("g"): ')		
-		
